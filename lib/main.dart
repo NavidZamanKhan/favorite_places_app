98af952b44f10fa53d1ef0b9975801e63eeda7ed
NavidZamanKhan <navid166418@gmail.com>
2025-06-21 21:47:42 +0600

    Replace HomeScreen with PlaceListScreen and implement Place model with UUID

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:favorite_places_app/screens/home_screen.dart';
+import 'package:favorite_places_app/screens/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const PlaceListScreen(),
     );
   }
 }
