a5fe1fcde8e3ba39be47f82410731d308b064b2e
NavidZamanKhan <navid166418@gmail.com>
2025-06-22 21:11:22 +0600

    Refactor places list implementation and update import paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:favorite_places_app/screens/places_list.dart';
+import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
