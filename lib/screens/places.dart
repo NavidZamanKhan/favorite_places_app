414a019619de04dd4b9bccb20a135bc4e39297d5
NavidZamanKhan <navid166418@gmail.com>
2025-06-23 23:16:40 +0600

    Refactor AddPlaceScreen and PlaceListScreen to use Riverpod for state management

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,12 +1,16 @@
+import 'package:favorite_places_app/providers/user_places.dart';
 import 'package:favorite_places_app/screens/add_place.dart';
 import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlaceListScreen extends StatelessWidget {
+class PlaceListScreen extends ConsumerWidget {
   const PlaceListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
