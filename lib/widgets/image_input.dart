875a2b652d68244e286d5380c71a127b4d36c4c3
NavidZamanKhan <navid166418@gmail.com>
2025-06-24 21:15:02 +0600

    Enhance ImageInput button style with rounded corners

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -17,6 +17,11 @@ class ImageInputState extends State<ImageInput> {
       width: double.infinity,
       alignment: Alignment.center,
       child: TextButton.icon(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(8),
+          ),
+        ),
         onPressed: () {},
         icon: const Icon(Icons.camera),
         label: const Text("Take Picture"),
