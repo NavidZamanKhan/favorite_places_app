5fab445308d8d8a82c0c27158bcf42324f695e94
NavidZamanKhan <navid166418@gmail.com>
2025-06-27 21:19:54 +0600

    Refactor PlacesList widget layout for improved styling and padding

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -24,26 +24,39 @@ class PlacesList extends StatelessWidget {
     return ListView.builder(
       itemCount: places.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          leading: CircleAvatar(
-            radius: 26,
-            backgroundImage: FileImage(File(places[index].image.path)),
-          ),
-          title: Text(
-            places[index].title,
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface,
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(color: Colors.black.withAlpha(20), blurRadius: 2),
+              ],
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 26,
+                backgroundImage: FileImage(File(places[index].image.path)),
+              ),
+              title: Text(
+                places[index].title,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) {
+                      return PlacesDetailScreen(place: places[index]);
+                    },
+                  ),
+                );
+              },
             ),
           ),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (ctx) {
-                  return PlacesDetailScreen(place: places[index]);
-                },
-              ),
-            );
-          },
         );
       },
     );
