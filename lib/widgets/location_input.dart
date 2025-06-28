f92fd8fb266161d037b610fde983d9a3576d019c
NavidZamanKhan <navid166418@gmail.com>
2025-06-29 00:54:33 +0600

    Enhance LocationInput widget with improved styling and layout for better user experience

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -16,19 +16,47 @@ class _LocationInputState extends State<LocationInput> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(),
+        Container(
+          height: 160,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+            ),
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            "No Location Chosen",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Theme.of(context).colorScheme.primary),
+          ),
+        ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
               onPressed: () {},
               label: const Text("Get Current Location"),
               icon: const Icon(Icons.location_on_outlined),
+              style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
             ),
             TextButton.icon(
               onPressed: () {},
               label: Text("Select on Map"),
               icon: Icon(Icons.map_outlined),
+              style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
             ),
           ],
         ),
