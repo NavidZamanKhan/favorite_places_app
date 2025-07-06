8a37a9fb73223dfbf6481b6c365fce56f8b0c91d
NavidZamanKhan <navid166418@gmail.com>
2025-07-06 22:38:14 +0600

    added const on placeholder

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,17 +1,29 @@
+import 'package:favorite_places_app/models/place.dart';
 import 'package:flutter/material.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({
+    super.key,
+    this.location = const PlaceLocation(
+      latitude: 37.4220041,
+      longitude: -122.0862462,
+      address: "",
+    ),
+    this.isSelecting = true,
+  });
+
+  final PlaceLocation location;
+  final bool isSelecting;
 
   @override
   State<MapScreen> createState() {
-    return MapScreenState();
+    return _MapScreenState();
   }
 }
 
-class MapScreenState extends State<MapScreen> {
+class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(appBar: AppBar());
   }
 }
